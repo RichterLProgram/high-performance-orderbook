a1da4c3f4df70e28f6f746edfd4198d6f7d2de55
RichterLProgram <RichterL.privat@gmail.com>
2025-11-27 17:22:01 +0100

    Orderbook Update

--- include/orderbook.hpp
+++ include/orderbook.hpp
@@ -5,14 +5,15 @@ struct Order {
     int id;
     double price;
     int quantity;
-    bool isBuyOrder;
+    bool isBuyOrder; // Differentiate between Buy/Sell orders
 };
 
 class OrderBook {
     public:
         void addOrder(const Order& order);
-        void matchOrders();
+        void matchOrders(); // Match buy and sell
     private:
-        std::map<double, std::queue<Order>, std::greater<>> buyOrders> buyOrders;
+        // Map find best prices quickly in O(log n) time
+        std::map<double, std::queue<Order>, std::greater<>> buyOrders;
         std::map<double, std::queue<Order>> sellOrders;
 };
